80ebfe0281e2483b2d784e4de2b8debfa9303b2c
ConteMan <boxiaozhi.bolu@gmail.com>
2021-07-11 16:04:19 +0800

    Initial commit

---
+++ customTypes/global.d.ts
@@ -0,0 +1,16 @@
+interface ImportMeta {
+    env: Record<string, unknown>;
+    globEager<T = unknown>(globPath: string): Record<string, T>;
+}
+
+interface memoryInfo {
+    jsHeapSizeLimit: number;
+    totalJSHeapSize: number;
+    usedJSHeapSize: number;
+}
+
+interface Window {
+    performance: {
+        memory: memoryInfo
+    }
+}
